c6281781229433b84c788cba0dc57ddcc479594b
Sung Wook <sungwook.yoo@kcpc.org>
2025-04-02 16:14:30 -0400

    session 09 assingment

---
+++ session09/assignment/user_list_nextjs/app/page.tsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+import {User} from "@/types";
+import UserNameList from "@/components/UserNameList";
+
+async function getUsers() {
+
+}
+
+export default async function Home() {
+
+  return (
+      </>
+  );
+}
